8ce4223bfd335a57b6d89161c77cb3af1cbd957f
Hellon Canella <helloncanella@gmail.com>
2019-09-19 10:58:16 -0300

    test is passing.

--- src/index.js
+++ src/index.js
@@ -1,43 +1,42 @@
-import _ from 'lodash';
-import { useQuery, useApolloClient } from '@apollo/react-hooks';
+import _ from "lodash"
+import { useQuery, useApolloClient } from "@apollo/react-hooks"
 
 export default function useAppState({ query, variables } = {}) {
-  if (_.isEmpty(query)) throw new Error('Missing query');
+  if (_.isEmpty(query)) throw new Error("Missing query")
 
   const { data } = useQuery(query, {
-    fetchPolicy: 'cache-only',
-    ...(variables ? { variables } : {}),
-  });
+    fetchPolicy: "cache-only",
+    ...(variables ? { variables } : {})
+  })
 
-  const queryField = getQueryField(query);
+  const queryField = getQueryField(query)
 
-  const appState = _.get(data, queryField);
-  const setAppState = useWriteQuery({ query, variables });
+  const appState = _.get(data, queryField)
+  const setAppState = useWriteQuery({ query, variables })
 
-  return [appState, setAppState];
+  return [appState, setAppState]
 }
 
 function useWriteQuery({ query, variables }) {
-  const client = useApolloClient();
-  const queryField = getQueryField(query);
-
+  const client = useApolloClient()
+  const queryField = getQueryField(query)
   return function writeQuery(data) {
     client.writeQuery({
       query,
       ...(variables ? { variables } : {}),
-      data: { [queryField]: data },
-    });
-  };
+      data: { [queryField]: data }
+    })
+  }
 }
 
 function getQueryField(query) {
-  const operation = _.get(query, 'definitions').find(
-    (q) => q.operation === 'query',
-  );
+  const operation = _.get(query, "definitions").find(
+    q => q.operation === "query"
+  )
 
-  if (!operation) throw new Error('You should pass an query operation');
+  if (!operation) throw new Error("You should pass an query operation")
 
-  const queryField = _.get(operation, 'selectionSet.selections.0.name.value');
+  const queryField = _.get(operation, "selectionSet.selections.0.name.value")
 
-  return queryField;
+  return queryField
 }
