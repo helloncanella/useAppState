2b5f893078f52d05c2ee4b0cd4ca32061e924f62
Hellon Canella <helloncanella@gmail.com>
2019-09-19 10:58:16 -0300

    test is passing

--- src/useAppState.test.js
+++ src/useAppState.test.js
@@ -1,19 +1,54 @@
-import { MockedProvider } from "@apollo/react-testing"
+import { MockedProvider, wait } from "@apollo/react-testing"
 import React from "react"
 import useAppState from "."
 import gql from "graphql-tag"
 import { mount } from "./test-utils/enzymeMounting"
+import { useQuery } from "@apollo/react-hooks"
+
+const QUERY = gql`
+  query Any {
+    hello @client
+  }
+`
 
 describe("useAppState", () => {
   test("it allows communication between two isolated components", async () => {
-    const valueA = ""
-    const setValueInB = Math.random() + ""
-
-    function Hello() {
-      return <h1>Hello</h1>
+    function ComponentA() {
+      const [value] = useAppState({ query: QUERY })
+      return <div data-value={value} />
     }
 
-    console.log(mount(<Hello></Hello>).debug())
+    function ComponentB() {
+      const [__, setValue] = useAppState({ query: QUERY })
+      return (
+        <input
+          type="text"
+          onChange={e => {
+            setValue(e.target.value)
+          }}
+        />
+      )
+    }
+
+    const el = mount(
+      <>
+        {/* two isolated components */}
+        <ComponentA></ComponentA>
+        <ComponentB></ComponentB>
+      </>
+    )
+
+    const elemB = el.find(ComponentB)
+
+    const setValueInB = Math.random() + ""
+    elemB.find("input").prop("onChange")({ target: { value: setValueInB } })
+
+    await wait(0)
+
+    el.update()
+
+    const elemA = el.find(ComponentA)
+    const valueA = elemA.find("div").prop("data-value")
 
     expect(valueA).toBe(setValueInB)
   })
